5da6d9ea079ce9623c19991ea0b8eeb8594a1856
DonTraffic <dogerfantom6@gmail.com>
2022-11-20 23:41:21 +0300

    add pagination v1

---
+++ components/Pagination.vue
@@ -0,0 +1,61 @@
+<template>
+    <section class="container pagination" v-if="data.info">
+        <div
+            class="pagination__button"
+            v-if="data.info.prev"
+            @click="paginationPush(page - 1)"
+        >
+            prev
+        </div>
+
+        <div
+            class="pagination__count"
+            v-if="page != 1"
+            @click="paginationPush(1)"
+        >
+            1
+        </div>
+
+        <div
+            class="pagination__count"
+            v-for="(count, index) in (data.info.pages - page) > 6 ? 5 : (data.info.pages - page)"
+            :key="count"
+            :class="{'active': index == 0 }"
+            @click="paginationPush(index + page)"
+        >
+            {{ index + page  }}
+        </div>
+
+        <div
+            class="pagination__count"
+            :class="{'active': page == data.info.pages}"
+            @click="paginationPush(data.info.pages)"
+        >
+            {{data.info.pages}}
+        </div>
+
+        <div
+            class="pagination__button"
+            v-if="data.info.next"
+            @click="paginationPush(page + 1)"
+        >
+            next
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Pagination',
+    props: {
+        data: Array,
+        page: Number,
+    },
+    methods: {
+        paginationPush(page) {
+            this.$emit('paginationGet', page);
+        },
+
+    }
+}
+</script>
--- pages/character.vue
+++ pages/character.vue
@@ -1,19 +1,23 @@
 <template>
-    <section class="container carts">
+    <div>
+        <section class="container carts">
 
-        <div class="carts__listing" v-if="data.results">
-            <div class="cart" v-for="item of data.results" :key="item.id">
-                <img :src="item.image" alt="img" class="cart__img">
-                <p class="cart__name">{{item.name}}</p>
+            <div class="carts__listing" v-if="data.results">
+                <div class="cart" v-for="item of data.results" :key="item.id">
+                    <img :src="item.image" alt="img" class="cart__img">
+                    <p class="cart__name">{{item.name}}</p>
+                </div>
             </div>
-        </div>
 
-        <div v-if="errors">
-            {{errors}}
-        </div>
+            <div v-if="errors">
+                {{errors}}
+            </div>
 
+        </section>
 
-    </section>
+        <Pagination :data="data" @paginationGet="pageGet" :page='page'/>
+
+    </div>
 </template>
 
 <script>
@@ -22,13 +26,34 @@ export default {
     data() {
         return {
             data: [],
-            errors: ''
+            errors: '',
+
+            page: 42,
         }
     },
+
     created: async function () {
-        await this.$axios.$get('https://rickandmortyapi.com/api/character') 
-            .then(resp => this.data = resp)
-            .catch(err => this.errors = err.response.data.error);
+        await this.getData()
+    },
+
+    methods: {
+        async getData() {
+            console.log('get');
+            await this.$axios.$get(`https://rickandmortyapi.com/api/character?page=${this.page}`) 
+                .then(resp => this.data = resp)
+                .catch(err => this.errors = err.response.data.error);
+        },
+
+        pageGet(page) {
+            this.page = page
+        }
+
+    },
+
+    watch: {
+        page() {
+            this.getData()
+        }
     }
 }
 </script>
